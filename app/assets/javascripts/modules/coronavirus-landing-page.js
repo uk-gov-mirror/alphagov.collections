2765686b5d6eee909e0693445630dc572c8daf6c
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2020-03-25 16:41:03 +0000

    for testing

--- app/assets/javascripts/modules/coronavirus-landing-page.js
+++ app/assets/javascripts/modules/coronavirus-landing-page.js
@@ -7,7 +7,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   Modules.CoronavirusLandingPage = function () {
     this.start = function () {
       // Confirm the user is on the coronavirus landing page
-      if (this.checkOnLandingPage()) {
+      if (window.location.pathname === "/coronavirus") {
         if (GOVUK.cookie("global_bar_seen")) {
           // Get current version of global bar, if cookie has been set
           var currentBannerVersion = JSON.parse(GOVUK.cookie("global_bar_seen")).version
@@ -17,9 +17,5 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         }
       }
     }
-  },
-
-  this.checkOnLandingPage = function () {
-    return window.location.pathname === "/coronavirus"
   }
 })(window.GOVUK.Modules)
